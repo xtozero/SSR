de329b91bd95ace06132e7b9c5dbced1d4d69cd6
xtozero <xtozero@naver.com>
2015-11-29 23:48:26 +0900

    스크립트 적용

---
+++ shared/Util.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+
+#include "Util.h"
+#include <tchar.h>
+
+namespace UTIL
+{
+	void Split( const String& string, std::vector<String>& params, const TCHAR token )
+	{
+		auto i = 0;
+		auto pos = string.find( token );
+		while ( pos != String::npos ) {
+			params.push_back( string.substr( i, pos - i ) );
+			i = ++pos;
+			pos = string.find( token, pos );
+
+			if ( pos == String::npos )
+			{
+				params.push_back( string.substr( i, string.length( ) - i ) );
+			}
+		}
+	}
+
+	void SplitByBracket( const String& string, std::vector<String>& params, const TCHAR startToken, const TCHAR endToken )
+	{
+		auto curIdx = 0;
+		auto startPos = string.find( startToken, curIdx );
+		auto endPos = string.find( endToken, ++startPos );
+
+		while ( startPos != String::npos && endPos != String::npos )
+		{
+			params.push_back( string.substr( startPos, endPos - startPos ) );
+			curIdx = ++endPos;
+
+			startPos = string.find( startToken, curIdx );
+			if ( startPos != String::npos )
+			{
+				endPos = string.find( endToken, ++startPos );
+			}
+		}
+	}
+}
