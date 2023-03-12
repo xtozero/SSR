abee1a793d0668d2b7b8474d89b30dd7bd988b1b
xtozero <xtozero@naver.com>
2023-03-12 16:48:31 +0900

    Rename RESOURCE_SECTION_DATA to ResourceSectionData

--- Source/Agl/Public/D3D11/D3D11BaseTexture.h
+++ Source/Agl/Public/D3D11/D3D11BaseTexture.h
@@ -27,7 +27,7 @@ namespace agl
 				m_initData.resize( numSections );
 				for ( size_t i = 0; i < numSections; ++i )
 				{
-					const RESOURCE_SECTION_DATA& section = initData->m_sections[i];
+					const ResourceSectionData& section = initData->m_sections[i];
 
 					m_initData[i].pSysMem = static_cast<unsigned char*>( m_dataStorage ) + section.m_offset;
 					m_initData[i].SysMemPitch = static_cast<uint32>( section.m_pitch );
